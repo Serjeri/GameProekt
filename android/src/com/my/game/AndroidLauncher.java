d15610d7e4339648a965d4e6a2c3c18700988471
Serjeri <hohachka@mail.ru>
2020-03-27 14:44:08 +0300

    Сделал другой фон

--- android/src/com/my/game/AndroidLauncher.java
+++ android/src/com/my/game/AndroidLauncher.java
@@ -7,17 +7,10 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.my.game.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
-
 	@Override
-
 	protected void onCreate (Bundle savedInstanceState) {
-
 		super.onCreate(savedInstanceState);
-
-
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-
 		initialize(new MyGdxGame(), config);
 	}
-
 }
