03e25eeac480f8fc75331e920210da350b5e242e
Serjeri <hohachka@mail.ru>
2020-03-27 16:56:10 +0300

    фон добавлен

--- core/src/com/my/game/MyGdxGame.java
+++ core/src/com/my/game/MyGdxGame.java
@@ -5,23 +5,30 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture imgFon;
+	TextureRegion region;
+	TextureRegion regionFon;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		imgFon = new Texture("fon.jpg");
+		region = new TextureRegion(img, 20,25,150,100);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0.5f, 0.3f, 0.1f, 1);
+		Gdx.gl.glClearColor(1,0,0,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img,  200, 135 );
+		batch.draw(imgFon,  0, 0 ,700,700);
 		batch.end();
 	}
 	
