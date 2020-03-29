b044dd69cea0b33a3093242a6683cc65ce6bdf13
Serjeri <hohachka@mail.ru>
2020-03-29 20:52:27 +0300

    Урок  2

--- core/src/com/my/game/Base/BaseScreen.java
+++ core/src/com/my/game/Base/BaseScreen.java
@@ -1,10 +1,12 @@
 package com.my.game.Base;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class BaseScreen implements Screen {
+public class BaseScreen implements Screen, InputProcessor {
 
     protected SpriteBatch batch;
 
@@ -13,6 +15,7 @@ public class BaseScreen implements Screen {
     public void show() {
         System.out.println("show");
         batch = new SpriteBatch();
+        Gdx.input.setInputProcessor(this);
 
     }
 
@@ -46,5 +49,52 @@ public class BaseScreen implements Screen {
     public void dispose() {
         System.out.println("dispose");
         batch.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        System.out.println("keyDown = " + keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        System.out.println("keyUp = " + keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        System.out.println("keyTyped =" + character);
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        System.out.println("touchDown = " + screenX + " y =" + screenY);
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        System.out.println("touchUp = " + screenX + " y =" + screenY);
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        System.out.println("touchDragged = " + screenX + " y =" + screenY);
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        System.out.println("scrolled = " + amount);
+        return false;
     }
 }
