c3f88db2577aba0928c6d003956d029f989ebceb
Serjeri <hohachka@mail.ru>
2020-03-30 21:18:17 +0300

    Домашние задание №3

--- core/src/com/my/game/sprite/MyClass.java
+++ core/src/com/my/game/sprite/MyClass.java
@@ -1,5 +1,6 @@
 package com.my.game.sprite;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.my.game.Base.Sprite;
@@ -7,29 +8,39 @@ import com.my.game.math.Rect;
 
 public class MyClass extends Sprite {
 
+    private static final float V_len = 0.01f;
     private Vector2 touch;
     private Vector2 V;
+    private Vector2 Buf;
 
-    public MyClass(TextureRegion region) {
-        super(region);
+    public MyClass(Texture region) {
+        super(new TextureRegion(region));
+
         touch = new Vector2();
         V = new Vector2();
+        Buf = new Vector2();
     }
 
     @Override
     public void resize(Rect worldBounds) {
-       setHeightProportion(0.5f);
+        setHeightProportion(0.4f);
     }
 
     @Override
     public void touchDown(Vector2 touch, int pointer, int button) {
-        super.touchDown(touch, pointer, button);
-        touch.set();
-        touch.set((touch));
+        this.touch.set(touch);
+        V.set(touch.sub(pos)).setLength(V_len);
     }
 
     @Override
     public void update(float delta) {
-        super.update(delta);
+
+        Buf.set(touch);
+
+        if (Buf.sub(pos).len() > V_len){
+            pos.add(V);
+        }else{
+            pos.set(touch);
+        }
     }
 }
