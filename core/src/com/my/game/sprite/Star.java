9f2946fcca5f46935b0e05b80cadc1e7561121aa
Serjeri <hohachka@mail.ru>
2020-03-31 15:47:32 +0300

    Урок № 4

--- core/src/com/my/game/sprite/Star.java
+++ core/src/com/my/game/sprite/Star.java
@@ -8,19 +8,24 @@ import com.my.game.math.Rnd;
 
 public class Star extends Sprite {
 
+    private static  final float start_heigh = 0.007f;
+
     private final Vector2 v;
     private Rect worldBounds;
+
+    private float anime_time;
+    private float anime_interval = 1f;
 
     public Star(TextureAtlas atlas) {
         super(atlas.findRegion("star"));
         v = new Vector2();
         v.set(Rnd.nextFloat(-0.005f,0.005f), Rnd.nextFloat(-0.1f,-0.01f));
-
+        anime_time = Rnd.nextFloat(0,1f);
     }
 
     @Override
     public void resize(Rect worldBounds) {
-        setHeightProportion(0.01f);
+        setHeightProportion(start_heigh);
         float posX = Rnd.nextFloat(worldBounds.getLeft(), worldBounds.getRight());
         float posY = Rnd.nextFloat(worldBounds.getBottom(), worldBounds.getTop());
         pos.set(posX , posY);
@@ -39,5 +44,14 @@ public class Star extends Sprite {
         }if(getBottom() > worldBounds.getTop()){
             setTop(worldBounds.getBottom());
         }
+        anime_time += delta;
+
+        if(anime_time >= anime_interval){
+            anime_time = 0;
+            setHeightProportion(start_heigh);
+        }else {
+            setHeightProportion(getHeight() + 0.0001f);
+        }
     }
+
 }
